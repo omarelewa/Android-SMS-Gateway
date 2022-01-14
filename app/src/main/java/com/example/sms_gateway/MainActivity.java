c520fce9413a819439f3e47b67424f1502c1a448
omar <elewa97@aucegypt.edu>
2022-01-14 23:21:41 +0200

    Final

--- app/src/main/java/com/example/sms_gateway/MainActivity.java
+++ app/src/main/java/com/example/sms_gateway/MainActivity.java
@@ -1,14 +1,167 @@
 package com.example.sms_gateway;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.sms_gateway.SendSMS;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
+    private Button btnRequest;
+
+    private RequestQueue mRequestQueue;
+    private StringRequest mStringRequest;
+    private String url = "https://google.com/";//"http://www.mocky.io/v2/597c41390f0000d002f4dbd1";
+
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
+    TextView tv_prompt;
+    TextView tv_sent_number;
+
+    Button sendBtn;
+    Button button_stop;
+
+    String phone_number;
+    String message;
+
+    int start_controller = 1;
+    int stop_controller = 0;
+
+    SendSMS mSender = new SendSMS();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sendBtn = (Button) findViewById(R.id.button_start);
+        button_stop = (Button) findViewById(R.id.button_stop);
+
+        tv_sent_number = (TextView) findViewById(R.id.tv_sent_number);
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //sendSMSMessage();
+                sendAndRequestResponse();
+
+                sendBtn.setVisibility(View.INVISIBLE);
+                Button stop = (Button) findViewById(R.id.button_stop);
+                stop.setVisibility(View.VISIBLE);
+            }
+        });
+
+        button_stop.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //sendSMSMessage();
+                button_stop.setVisibility(View.INVISIBLE);
+                Button start = (Button) findViewById(R.id.button_start);
+                start.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    private void sendAndRequestResponse() {
+
+        //RequestQueue initialized
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        //String Request initialized
+        mStringRequest = new StringRequest(
+                Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+
+                    @Override
+            public void onResponse(String response) {
+
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Response :"
+                                + response.toString(),
+                        Toast.LENGTH_LONG).show();//display the response on screen
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.i(TAG,"Error :" + error.toString());
+            }
+        });
+
+        mRequestQueue.add(mStringRequest);
+    }
+
+
+    protected void sendSMSMessage() {
+        phone_number = "5554";
+        message = "Heyyyy";
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS)) {
+            }
+
+            else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{
+                                Manifest.permission.SEND_SMS},
+                        MY_PERMISSIONS_REQUEST_SEND_SMS);
+            }
+        }
+        else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{
+                            Manifest.permission.SEND_SMS},
+                    MY_PERMISSIONS_REQUEST_SEND_SMS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions,
+                                           int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_SEND_SMS:
+                {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(phone_number, null, message, null, null);
+                    Toast.makeText(getApplicationContext(), "SMS sent.",
+                            Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),
+                            "SMS failed, please try again.", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 }
